8692e0d2095b089b6f6ad8ef080d1aca4848e03e
YimTaeKeun <ytk030305@gmail.com>
2025-02-18 20:09:00 +0900

    INIT: Initialize authenticate app

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework'
+    'rest_framework',
+    'authenticate'
 ]
 
 MIDDLEWARE = [
