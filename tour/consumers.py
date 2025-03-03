cc22096c57c439ff31ba15a2343825829501db27
YimTaeKeun <ytk030305@gmail.com>
2025-03-03 22:03:14 +0900

    CHORE: Fixed Comment

--- tour/consumers.py
+++ tour/consumers.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class TaskConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -10,9 +11,12 @@ class TaskConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
+        # Celery 작업 실행 (B 컨테이너에서 실행됨)
+        # async_to_sync(celery_app.send_task)("your_app.tasks.long_running_task", args=[self.task_id])
+
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def task_update(self, event):
-        # B 컨테이너에서 보낸 메시지를 클라이언트로 전송
+        # celery 컨테이너에서 보낸 메시지를 클라이언트로 전송
         await self.send(text_data=json.dumps(event["message"]))
