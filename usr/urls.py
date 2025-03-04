30073279841d530ee3b1c80e20fcb6a64bc4dbdc
jimmy914 <jimmy7620@naver.com>
2025-03-04 11:16:35 +0900

    urls.py 수정

--- usr/urls.py
+++ usr/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import Who
+from django.contrib import admin
+from django.urls import path, include  # include() 추가
 
 urlpatterns = [
-    path('user/me/', Who.as_view(), name='user_me'),
+    path('admin/', admin.site.urls),
+    path('usr/', include('usr.urls')),  # usr 앱의 urls.py 포함
 ]
