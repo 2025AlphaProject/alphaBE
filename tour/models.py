e5936132312ecad10961130cd538567aaae0ca71
YimTaeKeun <ytk030305@gmail.com>
2025-03-08 13:44:10 +0900

    FEAT: Event Model 추가

--- tour/models.py
+++ tour/models.py
@@ -6,6 +6,7 @@ from usr.models import User
 class Travel(models.Model):
     # id: pk
     user = models.ForeignKey(User, on_delete=models.CASCADE) # 유저 제거시 해당 여행도 제거
+    tour_name = models.CharField(max_length=255)  # 여행 이름 필드 추가
     start_date = models.DateField() # 여행 시작 날짜
     end_date = models.DateField() # 여행 마감 날짜
 
@@ -25,4 +26,16 @@ class PlaceImages(models.Model):
     # id: pk
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     image_url = models.URLField() # 이미지 url 정보를 저장합니다.
+
+class Event(models.Model):
+    category = models.CharField(max_length=100)
+    gu_name = models.CharField(max_length=100)
+    title = models.CharField(max_length=300)
+    img_url = models.URLField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    mapX = models.FloatField()
+    mapY = models.FloatField()
+    homepage_url = models.URLField()
+
 
