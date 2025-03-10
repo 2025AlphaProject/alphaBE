b7620edca1e7a78d1bf752f3bba45abccdd9a4d1
YimTaeKeun <ytk030305@gmail.com>
2025-03-10 23:18:02 +0900

    CHORE: mission db str 함수 수정 (#31)

--- mission/models.py
+++ mission/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Mission(models.Model): # 미션 모델을 생성합니다.
     # id: pk
     content = models.TextField() # 미션 내용 글자 수 제한 해제
+
+    def __str__(self): # 미션 생성시 오브젝트가 아닌 미션 이름이 보이도록 수정
+        return self.content
