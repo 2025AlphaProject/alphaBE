fd8b459e6425acf8ee64a4647533a86385a63257
YimTaeKeun <ytk030305@gmail.com>
2025-03-10 23:10:15 +0900

    FEAT: 미션 뷰 제작 완료 (#31)

--- mission/views.py
+++ mission/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Mission
+from .serializers import MissionSerializer
+
 
 # Create your views here.
+class MissionListView(viewsets.ModelViewSet):
+    queryset = Mission.objects.all()
+    serializer_class = MissionSerializer
