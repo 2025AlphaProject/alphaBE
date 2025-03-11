153f4aa391ab4c5c00354da134f38ef8c05f88fe
YimTaeKeun <ytk030305@gmail.com>
2025-03-11 09:28:17 +0900

    CHORE: CV 코드를 위한 서비스 파일 생성 및 주의사항 작성 (#35)

---
+++ mission/services.py
@@ -0,0 +1,14 @@
+"""
+    ISSUE - #36, ASSIGNEE - dbsthf04
+    이 파일에 CV 코드를 작성합니다.
+    함수 혹은 클래스 형태로 코드를 작성하여 Views.py에서 클래스나 함수 형태로 사용하여 결과값을 받을 수 있도록 해주시기 바랍니다.
+
+    <주의 사항>
+        1. 모듈 설치시 'pip install <모듈 이름>'으로 설치 후 반드시 'pip freeze > requirements.txt' 명령어를 입력하여 주시기 바랍니다.
+        2. 파이참 IDE 사용시 가상환경이 자동으로 설정되나, vscode와 같은 IDE를 사용하신다면 반드시 초기에 가상환경 설정 후
+        'pip install -r requirements.txt'를 해주시기 바랍니다.
+        3. 반드시 git flow 전략을 따르도록 하며, feature/#<이슈 번호>-<브랜치 이름> 형식의 브랜치를 파서 진행해주세요
+        4. 풀 리퀘스트를 생성할 때 source branch를 develop, target branch를 작업 브랜치로 해서 풀리퀘 생성해주세요.
+        5. 반드시 코드 리뷰어는 PM으로 해놓습니다.
+    TODO pip freeze > requirements.txt 명령어 반드시 입력
+"""
