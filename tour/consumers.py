9e63984e471dd91193361c64d551956a525b5e09
YimTaeKeun <ytk030305@gmail.com>
2025-03-13 20:31:46 +0900

    MODIFY: 경로 추천 지역 범위 17개 시/도로 수정 (#47)

--- tour/consumers.py
+++ tour/consumers.py
@@ -30,20 +30,22 @@ class TaskConsumer(AsyncWebsocketConsumer):
         # 요청을 celery task로 보냅니다.
         areaCode = params.pop('areaCode', [None])[0] # area_code 가져옴
         sigunguName = params.pop('sigunguName', [None])[0] # 시군구 이름 가져옴
-        if areaCode is None or sigunguName is None:
+        if areaCode is None: # areaCode가 존재하지 않는다면
             await self.send(text_data=json.dumps({
                 'state': 'ERROR',
                 'Message': '필수 파라미터 중 일부가 없습니다.'
             }, ensure_ascii=False))
             return
         tour = TourApi(MobileOS=MobileOS.ANDROID, MobileApp='AlphaProject2025', service_key=PUBLIC_DATA_PORTAL_API_KEY)
-        sigunguCode = tour.get_sigungu_code(areaCode, sigunguName) # 시군구 이름에 대응되는 코드를 가져옵니다.
-        if sigunguCode is None: # 시군구 코드가 없다면
-            await self.send(text_data=json.dumps({
-                'state': 'ERROR',
-                'Message': '해당 시군구 이름에 대응되는 코드를 가져올 수 없습니다. 시군구 이름을 다시 한번 확인 바랍니다.'
-            }, ensure_ascii=False))
-            return
+        sigunguCode = None
+        if sigunguName is not None:
+            sigunguCode = tour.get_sigungu_code(areaCode, sigunguName) # 시군구 이름에 대응되는 코드를 가져옵니다.
+            if sigunguCode is None: # 시군구 코드가 없다면
+                await self.send(text_data=json.dumps({
+                    'state': 'ERROR',
+                    'Message': '해당 시군구 이름에 대응되는 코드를 가져올 수 없습니다. 시군구 이름을 다시 한번 확인 바랍니다.'
+                }, ensure_ascii=False))
+                return
 
         task_result = app.send_task('tour.tasks.get_recommended_tour_based_area', args=[self.user_id, # 채널 레이어 그룹 특정을 위해 보냅니다.
                                                                                         areaCode, Arrange.TITLE_IMAGE.value, sigunguCode])
