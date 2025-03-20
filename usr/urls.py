d8e4e1ec3d8a79c886bfbfbfedba77e6ae72af3c
YimTaeKeun <ytk030305@gmail.com>
2025-03-20 09:44:18 +0900

    CHORE: 유저 리스트 api 엔드 포인트 추가 (#56)

--- usr/urls.py
+++ usr/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import Who
+from .views import Who, UserListView
 
 urlpatterns = [
+    path('', UserListView.as_view({
+        'get' : 'list',
+    }), name='user_list'),
     path('me/',Who.as_view({
         'get' : 'retrieve',
     }), name='who'),
