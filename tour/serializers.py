b332eea5c96b985e850754a93dc38dc11963ef4f
YimTaeKeun <ytk030305@gmail.com>
2025-03-20 10:57:16 +0900

    FIX: 유저 정보 read only 설정 (#56)

--- tour/serializers.py
+++ tour/serializers.py
@@ -7,6 +7,7 @@ class TravelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Travel
         fields = '__all__' #포한하는 필드 지정, id는 장고에서 자동으로 생성!하니까 필드에 넣어두는 것이당!
+        read_only_fields = ('user',)
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
