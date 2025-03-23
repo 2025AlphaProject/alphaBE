7ef05c25ae59ab770eea5076bed9d3d92210af4c
YimTaeKeun <ytk030305@gmail.com>
2025-03-23 21:02:46 +0900

    MODIFY: url 매핑 수정 (#59)

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import kakao_callback, KakaoRefreshTokens, IssueTokenView
+from .views import kakao_callback, KakaoRefreshTokens, LoginRegisterView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('login/', IssueTokenView.as_view(), name='login'), # 로그인 url 매핑
+    path('login/', LoginRegisterView.as_view({
+        'post': 'create',
+    }), name='login_register'),
+    path('get_token/', kakao_callback, name='login'), # 로그인 url 매핑
     path('refresh/', TokenRefreshView.as_view(), name='refresh_tokens'),
 ]
