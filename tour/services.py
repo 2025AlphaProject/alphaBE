ed71c3684240d753ceb002a382b212db03089866
YimTaeKeun <ytk030305@gmail.com>
2025-03-25 20:07:42 +0900

    FEAT: 지번 가져오기 서비스 클래스 추가 (#65)

--- tour/services.py
+++ tour/services.py
@@ -1,8 +1,12 @@
 import requests
 import json
+from config.settings import KAKAO_REST_API_KEY, APP_LOGGER
+import logging
+
+logger = logging.getLogger(APP_LOGGER)
 
 
-class GeocoderService:
+class PlaceService:
     """
     장소와 관련된 여러 서비스를 구현 합니다.
     """
@@ -16,29 +20,22 @@ class GeocoderService:
         if not self.service_key:
             raise Exception('Service Key is required')
 
-        response = self.__get_geocoder_response(
-            service='address',
-            request='getaddress',
-            point=f'{x},{y}',
-            format='json',
-            type='PARCEL',
-            key=self.service_key,
-        )
-        return response['result']['item']['text']
+        response = self.__get_kakao_response(x=x, y=y)
+        parcel_address = response['documents'][0]['address']['address_name']
+        if parcel_address is None:
+            logger.warning('There is no address')
+            return ""
+        return parcel_address
 
-    def __get_geocoder_response(self, **kwargs) -> json:
+    def __get_kakao_response(self, **kwargs) -> json:
         """
-        geocoder api의 응답을 받아오는 함수입니다.
+        kakao api의 응답을 받아오는 함수입니다.
         """
-        end_point = 'https://api.vworld.kr/req/address'
-        response = requests.get(end_point, params=kwargs)
+        end_point = 'https://dapi.kakao.com/v2/local/geo/coord2address.JSON'
+        headers = {'Authorization': f'KakaoAK {self.service_key}'}
+        response = requests.get(end_point, params=kwargs, headers=headers)
         if response.status_code != 200:
-            raise Exception('Geocoder API Error')
+            logger.error('kakao local api error (38)')
+            raise Exception('Kakao API Error')
 
         return response.json()
-
-
-if __name__ == '__main__':
-    place = GeocoderService('088ECB86-CAA5-3EF6-8CE0-FB640E00F20E')
-    address = place.get_parcel(x=129.053822, y=35.25138)
-    print(address)
