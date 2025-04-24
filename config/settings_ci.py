6545f4371bc88737237274601933f5b68a068f49
YimTaeKeun <ytk030305@gmail.com>
2025-04-24 10:01:13 +0900

    FIX: ci 관련 코드 수정

--- config/settings_ci.py
+++ config/settings_ci.py
@@ -1,9 +1,9 @@
-import config.settings as settings
+from config.settings import *
 
 # 깃허브 ci 테스트를 위해 아래와 같이 DB를 설정합니다.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': settings.BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
