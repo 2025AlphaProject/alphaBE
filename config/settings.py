d084f2b2e37910c6a40fb4e769200c7893d3771c
YimTaeKeun <ytk030305@gmail.com>
2025-05-16 17:59:53 +0900

    CHORE: 예외 미들웨어 제거 (#76)

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'middleware.request_logger.RequestLogMiddleware',
-    'middleware.exception_middleware.ExceptionMiddleware',
 ]
 
 # TODO 특정 호스트만 접속하도록 허용할것
