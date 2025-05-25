0721a9609f8be10eea475459f274d4737f8bb686
YimTaeKeun <ytk030305@gmail.com>
2025-05-25 23:36:14 +0900

    FEAT: refresh token 뷰 추가 (#115)

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import kakao_callback, KakaoRefreshTokens, LoginRegisterView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('login/', LoginRegisterView.as_view({
         'post': 'create',
     }), name='login_register'),
     path('get_token/', kakao_callback, name='login'), # 로그인 url 매핑
-    path('refresh/', KakaoRefreshTokens.as_view({
-        'post': 'create',
-    }), name='refresh_tokens'),
+    # path('refresh/', KakaoRefreshTokens.as_view({
+    #     'post': 'create',
+    # }), name='refresh_tokens'),
+    path('refresh/', TokenRefreshView.as_view())
 ]
