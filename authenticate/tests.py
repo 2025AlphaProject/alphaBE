f84a03735b8ee23abb63d18ee26345de65ca92da
YimTaeKeun <ytk030305@gmail.com>
2025-07-31 00:16:42 +0900

    MODIFY: 로그인 테스트 수정 (#140)

--- authenticate/tests.py
+++ authenticate/tests.py
@@ -53,7 +53,6 @@ class TestAuthenticate(BaseTestCase):
         response = self.client.post(target_uri, data=data, content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
-    @unittest.skipIf(SKIP_TEST == 'True', "Skip Login Test")
     def test_login(self):
         """
         해당 함수는 flutter sdk로 발급받은 액세스 토큰과 아이디 토큰을 활용하여 로그인 혹은 회원가입 진행이 되는지 확인합니다.
@@ -66,9 +65,8 @@ class TestAuthenticate(BaseTestCase):
             'id_token': self.KAKAO_TEST_ID_TOKEN,
         }
         # register Test
-        response = self.client.post(end_point, headers=headers, data=data, content_type='application/json')
+        response = self.client.post(end_point, data=data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json()['is_new'], True)
 
         # login Test
         response = self.client.post(end_point, headers=headers, data=data, content_type='application/json')
