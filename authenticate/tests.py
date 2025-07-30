2bbbdb62d50871295fd79c2f938656087d535eb7
YimTaeKeun <ytk030305@gmail.com>
2025-07-31 00:54:49 +0900

    TEST: 로그인 및 refresh token 테스트 수정 (#140)

--- authenticate/tests.py
+++ authenticate/tests.py
@@ -1,15 +1,19 @@
-import  unittest
-from django.test import TestCase
+import logging
+import unittest
+
+from django.test import override_settings
+from django.urls import reverse
+
 from config.settings import (
-    KAKAO_AUTH_CODE, # 임시 인가 코드를 가져옵니다. 테스트 실행시마다 .env 파일에서 매번 바꿔줘야합니다.
-    KAKAO_REFRESH_TOKEN, # 리프레시 토큰. 만료시 바꿔 사용
+    KAKAO_AUTH_CODE,  # 임시 인가 코드를 가져옵니다. 테스트 실행시마다 .env 파일에서 매번 바꿔줘야합니다.
+    # 리프레시 토큰. 만료시 바꿔 사용
     APP_LOGGER,
     SKIP_TEST,
-    KAKAO_REST_API_KEY,
+    SIMPLE_JWT,
+    REFRESH_TOKEN,
 )
-import logging
-from services.kakao_token_service import KakaoTokenService
 from tests.base import BaseTestCase
+
 logger = logging.getLogger(APP_LOGGER)
 
 # Create your tests here.
@@ -27,37 +31,51 @@ class TestAuthenticate(BaseTestCase):
         print(response.json())
         self.assertEqual(response.status_code, 201)
 
-    @unittest.skipIf(SKIP_TEST == 'True', "Skip Login Refresh Test")
-    def test_refresh_token(self):
+    # python의 unpacking은 같은 값이 있다면 덮어쓰기로 진행이 됨. 따라서 BLACKLIST_AFTER_ROTATION값은 False로 됨
+    # 아래 설정은 리프레시 토큰은 테스트 중에는 블랙리스트에 넣지 않도록 함
+    @override_settings(SIMPLE_JWT={**SIMPLE_JWT, "BLACKLIST_AFTER_ROTATION": False})
+    def test_refresh_token_success(self):
         """
-        해당 테스트는 카카오 리프레시 토큰이 제대로 날라오는지 확인하기 위한 테스트입니다.
+            해당 테스트는 토큰이 제대로 refresh가 되는지 테스트합니다.
         """
-        target_uri = '/auth/refresh/'
-        # TODO 리프레시 토큰으로서 유효기간이 만료되면 바꿔줘야합니다.
+        uri = reverse('refresh')
         data = {
-            'refresh_token': KAKAO_REFRESH_TOKEN,
+            'refresh_token': REFRESH_TOKEN,
         }
-        response = self.client.post(target_uri, data=data, content_type='application/json')
-        self.assertEqual(response.status_code, 201)
-        # 리프레시 토큰 정보를 body에서 발견하지 못한 경우를 테스트 합니다.
+        response = self.client.post(uri, data=data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        logger.debug('test_refresh_token_success result:' + str(response.json()))
+
+    def test_refresh_token_failure_no_parameter(self):
+        """
+            해당 테스트는 refresh token 뷰에서 파라미터가 제대로 날라오지 않았을 경우를 테스트합니다.
+        """
+        uri = reverse('refresh')
         data = {
-            # 일부러 틀린 정보를 넣습니다.
-            'ref': '',
+            'ref': '123sdf1',
         }
-        response = self.client.post(target_uri, data=data, content_type='application/json')
+        response = self.client.post(uri, data=data, content_type='application/json')
         self.assertEqual(response.status_code, 400)
-        # 토큰 정보 자체를 변환해서 테스트합니다.
+        logger.debug('refresh no parameter result:' + str(response.json()))
+
+    def test_refresh_token_failure_invalid_token(self):
+        """
+            해당 테스트는 refresh token 뷰에서 토큰이 유효하지 않은 경우를 테스트합니다.
+        """
+        uri = reverse('refresh')
         data = {
             'refresh_token': '1pbZZHeOq9TsJBPQgA-URNdOUoDlhxp__AAAAAgo9cusAAAGUKtVQgeQ1KlcE_6bt',
         }
-        response = self.client.post(target_uri, data=data, content_type='application/json')
+        response = self.client.post(uri, data=data, content_type='application/json')
         self.assertEqual(response.status_code, 400)
+        logger.debug('refresh invalid token result:' + str(response.json()))
 
-    def test_login(self):
+
+    def test_login_success(self):
         """
         해당 함수는 flutter sdk로 발급받은 액세스 토큰과 아이디 토큰을 활용하여 로그인 혹은 회원가입 진행이 되는지 확인합니다.
         """
-        end_point = '/auth/login/'
+        end_point = reverse('login_register')
         headers = {
             'Authorization': f'Bearer {self.KAKAO_TEST_ACCESS_TOKEN}'
         }
@@ -72,11 +90,20 @@ class TestAuthenticate(BaseTestCase):
         response = self.client.post(end_point, headers=headers, data=data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json()['is_new'], False)
+        logger.debug('login success result:' + str(response.json()))
 
+    def test_login_failure(self):
+        """
+            로그인 실패를 테스트합니다.
+        """
+        end_point = reverse('login')
+        headers = {
+            'Authorization': f'Bearer {self.KAKAO_TEST_ACCESS_TOKEN}'
+        }
         # 400 Test
         data2 = {
-            'id_token' : 'hsesefs'
+            'id_token': 'hsesefs'
         }
         response = self.client.post(end_point, headers=headers, data=data2, content_type='application/json')
         self.assertEqual(response.status_code, 400)
-
+        logger.debug('login failure result:' + str(response.json()))
