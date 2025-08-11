8f849c337bda9e3e31c4bccf93f6fd2ba5dcc92f
YimTaeKeun <ytk030305@gmail.com>
2025-08-11 20:44:01 +0900

    MODIFY: fcm token을 wirte only로 설정 (#156)

--- usr/serializers.py
+++ usr/serializers.py
@@ -4,6 +4,7 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    fcm_token = serializers.CharField(write_only=True)
     class Meta:
         model = User
         fields = ['sub', 'username', 'profile_image_url', 'fcm_token']
