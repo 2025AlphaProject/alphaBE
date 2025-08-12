6d2d772b281807d3b6e9b3c765ed5193d28a0176
siyeon <jimmy7620@naver.com>
2025-08-12 21:08:38 +0900

    settings 수정

--- config/settings.py
+++ config/settings.py
@@ -319,7 +319,7 @@ MODEL_DIR = os.path.join(BASE_DIR, "mission", "yolomodels")
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': f'redis://{env('CHANNEL_HOST')}:6379/1',
+        'LOCATION': f'redis://{env("CHANNEL_HOST")}:6379/1',
         'OPTION': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             'TIMEOUT': 600, # cache 유효 기간을 10분으로 설정합니다.
